2e3b708cb2286aac24ecc8e1abc3026d301d0436
vinay12050 <vinay12050@gmail.com>
2021-12-11 16:06:16 +0530

    added 1 file

---
+++ UserLogin.java
@@ -0,0 +1,19 @@
+package com.facebook.instagram.login;
+
+import com.facebook.instagram.login.User;
+
+public class UserLogin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Main method started");
+		
+      User user=new User();
+      user.login("Vinay", null);   
+
+System.out.println("Main method ended");
+
+	}
+
+}
